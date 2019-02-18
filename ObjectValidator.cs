9cb268fb3b3fd51ec2b412895e4916d758754097
cole.miller218 <31394408+ColeTMiller@users.noreply.github.com>
2019-02-18 13:30:44 -0600

    Add files via upload

---
+++ ObjectValidator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq; 
+using System.Text;
+using System.Threading.Tasks; 
+
+namespace MedID.Lib
+{
+    public  static class ObjectValidator
+    {
+        public static void ValidateObject(IValidatableObject value)
+        {
+            Validator.ValidateObject(value, new ValidationContext(value), true); 
+
+        }
+        public static IEnumerable<ValidationResult>TryValidateObject(IValidatableObject value)
+        {
+            var results = new List<ValidationResult>();
+            if (Validator.TryValidateObject(value, new ValidationContext(value), results))
+                return results;
+
+            return Enumerable.Empty<ValidationResult>(); 
+        }
+    }
+}
